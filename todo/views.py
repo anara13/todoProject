ee3425594e961c14f7bf20c2dc5e622ece960069
RASOLOFOHARISON <rasolofohariso@gmail.com>
2022-11-16 20:30:11 +0100

    Display of To Dos in the database done

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ToDo
 
 # Create your views here.
+
+class ListListView(ListView):
+    model = ToDo
+    template_name = "todo/index.html"

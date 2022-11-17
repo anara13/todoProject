01ffd67781f848a341bd575749e07dd5b527fa5a
RASOLOFOHARISON <rasolofohariso@gmail.com>
2022-11-17 23:16:37 +0100

    delete done

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.ToDoView.as_view(), name="index"),
+    path('', views.ToDoView.as_view(), name="index"),
     path("addToDo/", views.ToDoCreate.as_view(), name="add-To-Do"),
+    path("delete/<int:pk>/", views.ToDoDelete.as_view(), name="delete-To-Do"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.urls import  reverse_lazy
 from django.views.generic import ListView
 from django.views.generic import CreateView
+from django.views.generic import DeleteView
 
 from .models import ToDo
 
@@ -17,5 +18,11 @@ class ToDoCreate(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super(ToDoCreate, self).get_context_data()
-        context["title"] = "Add a new ToDo"
+        context["title"] = "Add a new To Do"
         return context
+
+class ToDoDelete(DeleteView):
+    model = ToDo
+    template_name = "todo/deleteToDo.html"
+
+    success_url = reverse_lazy("index")
